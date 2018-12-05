20806d1927237500461b5496fe0573710d2cde4c
Bernard Huff <bernard.huff008@gmail.com>
2018-12-05 14:51:21 -0600

    Init commit

---
+++ Cardie/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Cardie
+//
+//  Created by Bernard Huff on 12/5/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
