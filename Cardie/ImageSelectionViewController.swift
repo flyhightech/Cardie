88a4c6a915dc0ab767985ec06c126cbf47fbf063
Bernard Huff <bernard.huff008@gmail.com>
2018-12-23 17:30:51 -0600

    Sweep

---
+++ Cardie/ImageSelectionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ImageSelectionViewController.swift
+//  Cardie
+//
+//  Created by Bernard Huff on 12/23/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectionViewController: UIViewController {
+
+    @IBOutlet weak var initialImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
